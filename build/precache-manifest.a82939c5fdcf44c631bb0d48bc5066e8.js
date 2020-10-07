16cbdff056a8c831a8abed128df981c5ab0af19e
Jacob Burritt <rexro98@gmail.com>
2020-10-07 17:10:21 -0400

    Changed page title and icon

---
+++ build/precache-manifest.a82939c5fdcf44c631bb0d48bc5066e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35909abb3d2629d531f25a1cde482000",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5faafea66060ca7f50dc",
+    "url": "/static/css/main.4f386888.chunk.css"
+  },
+  {
+    "revision": "1d411ee13cec0824c2a7",
+    "url": "/static/js/2.8626492a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8626492a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5faafea66060ca7f50dc",
+    "url": "/static/js/main.654f6a38.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
